06d311ecad59c1073a4ef75302f9115f45253837
Redtronic <redtronicmc@gmail.com>
2026-05-11 12:51:07 +0200

    fix cors and url

--- extension/vite.config.ts
+++ extension/vite.config.ts
@@ -1,28 +1,30 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import { resolve } from "path"
 import { viteStaticCopy } from "vite-plugin-static-copy"
 
-export default defineConfig({
-	plugins: [
-		viteStaticCopy({
-			targets: [
-				{
-					src: "manifest.json",
-					dest: ".",
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "")
+
+	return {
+		plugins: [
+			viteStaticCopy({
+				targets: [{ src: "manifest.json", dest: "." }],
+			}),
+		],
+		define: {
+			"import.meta.env.VITE_API_URL": JSON.stringify(env.VITE_API_URL),
+		},
+		build: {
+			outDir: "dist",
+			emptyOutDir: true,
+			rollupOptions: {
+				input: {
+					content: resolve(__dirname, "src/content/index.ts"),
 				},
-			],
-		}),
-	],
-	build: {
-		outDir: "dist",
-		emptyOutDir: true,
-		rollupOptions: {
-			input: {
-				content: resolve(__dirname, "src/content/index.ts"),
-			},
-			output: {
-				entryFileNames: "[name].js",
+				output: {
+					entryFileNames: "[name].js",
+				},
 			},
 		},
-	},
+	}
 })
